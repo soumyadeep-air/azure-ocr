50ebbdb499b584f29c4128f520868ceee8a74337
soumyadeep-air <soumyadeep@airditsoftware.com>
2024-02-01 00:22:01 +0530

    removed commented code

--- ocr.js
+++ ocr.js
@@ -45,65 +45,7 @@ async function extract({
 
   const { content, pages, languages, styles, keyValuePairs } = await poller.pollUntilDone();
 
-  console.log(keyValuePairs);
   return { content, pages, languages, styles, keyValuePairs };
-
-  // if (pages.length <= 0) {
-  //   console.log("No pages were extracted from the document.");
-  // } else {
-  //   console.log("Pages:");
-  //   for (const page of pages) {
-  //     console.log("- Page", page.pageNumber, `(unit: ${page.unit})`);
-  //     console.log(`  ${page.width}x${page.height}, angle: ${page.angle}`);
-  //     console.log(`  ${page.lines.length} lines, ${page.words.length} words`);
-
-  //     if (page.lines.length > 0) {
-  //       console.log("  Lines:");
-
-  //       for (const line of page.lines) {
-  //         console.log(`  - "${line.content}"`);
-
-  //         // The words of the line can also be iterated independently. The words are computed based on their
-  //         // corresponding spans.
-  //         for (const word of line.words()) {
-  //           console.log(`    - "${word.content}"`);
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-
-  // if (languages?.length <= 0) {
-  //   console.log("No language spans were extracted from the document.");
-  // } else {
-  //   console.log("Languages:");
-  //   for (const languageEntry of languages) {
-  //     console.log(
-  //       `- Found language: ${languageEntry.languageCode} (confidence: ${languageEntry.confidence})`
-  //     );
-  //     for (const text of getTextOfSpans(content, languageEntry.spans)) {
-  //       const escapedText = text.replace(/\r?\n/g, "\\n").replace(/"/g, '\\"');
-  //       console.log(`  - "${escapedText}"`);
-  //     }
-  //   }
-  // }
-
-  // if (styles?.length <= 0) {
-  //   console.log("No text styles were extracted from the document.");
-  // } else {
-  //   console.log("Styles:");
-  //   for (const style of styles) {
-  //     console.log(
-  //       `- Handwritten: ${style.isHandwritten ? "yes" : "no"} (confidence=${
-  //         style.confidence
-  //       })`
-  //     );
-
-  //     for (const word of getTextOfSpans(content, style.spans)) {
-  //       console.log(`  - "${word}"`);
-  //     }
-  //   }
-  // }
 }
 
 // main().catch((error) => {

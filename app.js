642bafb1f1c9b6268b15ed35144cc8495b566bc5
soumyadeep-air <soumyadeep@airditsoftware.com>
2024-02-01 00:29:24 +0530

    changed port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { handleUpload, multerMiddleware } = require("./file-upload");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "public")));
 
